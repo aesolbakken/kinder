3197868ab3900b9c993973c47f9d2d97325d151f
Alf Solbakken <aesolbakken@gmail.com>
2012-03-04 11:54:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kinder::Application.config.secret_token = '036d039e67aab721ddd88754431ad304dc1e4a5713437368950efe6e84177dac16149ee2347efde8f40f7e7e1bd7d99947094b907ed08bea6519cea33eba7c45'
