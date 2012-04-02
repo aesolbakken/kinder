c99a28b942f536bea34fd12c193f30eb48971a93
Alf Solbakken <aesolbakken@gmail.com>
2012-04-02 20:59:05 +0200

    added mail verification

--- app/models/kindergarten.rb
+++ app/models/kindergarten.rb
@@ -4,4 +4,5 @@ class Kindergarten < ActiveRecord::Base
   has_many :departments, :dependent => :destroy
   accepts_nested_attributes_for :departments, :allow_destroy => true
   belongs_to :user
+  validates :email, :allow_blank => true, :uniqueness => {:case_sensitive => false}, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -2,4 +2,5 @@ class Parent < ActiveRecord::Base
   belongs_to :child
   attr_accessible :name, :phone, :email
   validates_presence_of :name, :phone
+  validates :email, :allow_blank => true, :uniqueness => {:case_sensitive => false}, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
